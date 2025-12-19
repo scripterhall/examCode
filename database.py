d54b1583d85d5b579522e7b56449c50edc576bf2
scripterhall <halloul.nourallah@gmail.com>
2025-12-20 00:32:23 +0100

    db conf

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user_name:password@endpoint/postgres"
+SQLALCHEMY_DATABASE_URL = "rev-db-ins.c3ge8uoamr38.us-east-1.rds.amazonaws.com"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
